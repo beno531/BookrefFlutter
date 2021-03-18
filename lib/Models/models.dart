4fb90c1ee0ec81bab6b49e092b3a4a9692bc28ef
Benito Ernst <benitoernst@MacBook-Pro-von-Benito.fritz.box>
2021-03-18 18:48:15 +0100

    update

--- lib/Models/models.dart
+++ lib/Models/models.dart
@@ -1,2 +1,8 @@
 export 'user.dart';
 export 'signin.dart';
+export 'testbook.dart';
+export 'recommendedPerson.dart';
+export 'recommendedBook.dart';
+export 'dashboardBooks.dart';
+export 'bookResult.dart';
+export 'book.dart';
